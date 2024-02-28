07416f4c2f00fdaa96fea5032ee5a6c164bf52f2
NpatsL <napatsorn.la@ku.th>
2024-02-28 17:54:38 +0700

    add rich-text editor

--- api/index.php
+++ api/index.php
@@ -35,6 +35,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.tiny.cloud/1/7gb95ur3z4720nibl33n5463t47byxjvx70z2k6ittg209a4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <script src="https://cdn.tiny.cloud/1/7gb95ur3z4720nibl33n5463t47byxjvx70z2k6ittg209a4/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
     <title>ตั้งกระทู้ใหม่</title>
 </head>
@@ -130,6 +133,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (window.history.replaceState) {
                 window.history.replaceState(null, null, window.location.href);
             }
+        </script>
+        <script>
+            tinymce.init({
+                selector: 'textarea',
+                plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+                toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                tinycomments_mode: 'embedded',
+                tinycomments_author: 'Author name',
+                mergetags_list: [{
+                        value: 'First.Name',
+                        title: 'First Name'
+                    },
+                    {
+                        value: 'Email',
+                        title: 'Email'
+                    },
+                ],
+                ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+            });
         </script>
 </body>
 
