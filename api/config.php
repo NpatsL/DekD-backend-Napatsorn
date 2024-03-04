2ef249a45f6098eefd9a29b44b62110df9c0f94f
NpatsL <napatsorn.la@ku.th>
2024-03-04 18:08:52 +0700

    update db

--- api/config.php
+++ api/config.php
@@ -1,18 +1,20 @@
 
 <?php
 
+
 class Config
 {
     private const DB_HOST = 'sql6.freesqldatabase.com';
-    private const DB_NAME = 'sql6686984';
-    private const DB_USER = 'sql6686984';
-    private const DB_PASSWORD = 'bgVXsDJsl3';
-    private $dsn = "mysql:host=" . self::DB_HOST . ";dbname=" . self::DB_NAME;
+    private const DB_NAME = 'sql6688578';
+    private const DB_USER = 'sql6688578';
+    private const DB_PASSWORD = 'yl8pZFLcsw';
+    private $dsn = 'mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME . ';sslaccept=strict';
     protected $conn = null;
 
     public function __construct()
     {
         try {
+            
             $this->conn = new PDO($this->dsn, self::DB_USER, self::DB_PASSWORD);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
